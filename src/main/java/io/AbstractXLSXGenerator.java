5ee6196f8a50f392ff621d96a2d46108930fc177
fniyaz <35842362+fniyaz@users.noreply.github.com>
2019-05-03 02:49:51 +0300

    Basic generation is done

--- src/main/java/io/AbstractXLSXGenerator.java
+++ src/main/java/io/AbstractXLSXGenerator.java
@@ -2,28 +2,25 @@ package io;
 
 import algorithms.TimeTable;
 import algorithms.YearGroup;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.*;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 
 public abstract class AbstractXLSXGenerator {
     private XSSFWorkbook wb = new XSSFWorkbook();
     private XSSFSheet sheet = wb.createSheet(getScheduleSheetName());
+    private Map<Integer, XSSFCellStyle> styles = new HashMap<>();
 
 
-    protected XSSFCellStyle centred;
-
-    protected AbstractXLSXGenerator() {
-        setUpStyles();
-    }
 
     public abstract void generate(String path, TimeTable tt, Map<String, YearGroup> groups) throws IOException;
-
     protected abstract String getScheduleSheetName();
 
 
@@ -47,8 +44,10 @@ public abstract class AbstractXLSXGenerator {
             row = sheet.createRow(r);
 
         XSSFCell cell = row.getCell(c);
-        if (cell == null)
+        if (cell == null) {
             cell = row.createCell(c);
+            sheet.autoSizeColumn(c);
+        }
 
         if (style != null)
             cell.setCellStyle(style);
@@ -56,13 +55,47 @@ public abstract class AbstractXLSXGenerator {
             cell.setCellValue(content);
     }
 
-    protected void setUpStyles() {
-        centred = wb.createCellStyle();
-        centred.setAlignment(HorizontalAlignment.CENTER);
-    }
+    protected XSSFCellStyle style(Style... style){
+        int tb = 0;
+        for (Style s: style)
+            tb |= s.style;
 
+        if (styles.get(tb) == null) {
+            XSSFCellStyle cellStyle = wb.createCellStyle();
+            for (Style s: style){
+                s.apply.accept(cellStyle);
+            }
+            styles.put(tb, cellStyle);
+        }
+
+        return styles.get(tb);
+    }
 
     protected void save(OutputStream file) throws IOException {
         wb.write(file);
+    }
+
+
+    private static int auto = 1;
+    protected enum Style{
+        CENTER(auto<<=1, a -> {a.setAlignment(HorizontalAlignment.CENTER);
+            a.setVerticalAlignment(VerticalAlignment.CENTER);}),
+
+        B_BOTTOM(auto<<=1, a -> a.setBorderBottom(BorderStyle.MEDIUM)),
+        B_TOP(auto<<=1, a -> a.setBorderTop(BorderStyle.MEDIUM)),
+        B_LEFT(auto<<=1, a -> a.setBorderLeft(BorderStyle.MEDIUM)),
+        B_RIGHT(auto<<=1, a -> a.setBorderRight(BorderStyle.MEDIUM)),
+        BG_GREEN(auto<<=1, a -> {a.setFillForegroundColor(IndexedColors.LIGHT_GREEN.getIndex());
+        a.setFillPattern(FillPatternType.SOLID_FOREGROUND);}),
+        BG_BLUE(auto<<=1, a -> {a.setFillForegroundColor(IndexedColors.CORNFLOWER_BLUE.getIndex());
+            a.setFillPattern(FillPatternType.SOLID_FOREGROUND);})
+        ;
+
+        public int style;
+        public Consumer<XSSFCellStyle> apply;
+        Style(int style, Consumer<XSSFCellStyle> apply){
+            this.style = style;
+            this.apply = apply;
+        }
     }
 }
